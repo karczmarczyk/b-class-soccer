095453c8a4bdae00ebc1edc66c4a1a99f29e8ee3
Mateusz Karczmarczyk <mateusz.karczmarczyk@gmail.com>
2023-03-21 21:45:03 +0100

    goals

--- js/stadium.js
+++ js/stadium.js
@@ -6,9 +6,18 @@ class Stadium
     field = null;
     sideA = null;
     sideB = null;
+    
+    goalA = null;
+    goalPostA1 = null;
+    goalPostA2 = null;
+    
+    goalB = null;
+    goalPostB1 = null;
+    goalPostB2 = null;
 
     length = 200;
     width = 100;
+    goalSize = 25;
 
     centerPosition = {
         x: 0,
@@ -31,12 +40,30 @@ class Stadium
     create() {
         this.stadium = $('<div class="stadium"></div>');
         this.field = $('<div class="field"></div>');
+
+        this.goalA = this.createGoal("A");
+        this.goalB = this.createGoal("B");
+
         this.sideA = $('<div class="field-side sideA"></div>');
         this.sideB = $('<div class="field-side sideB"></div>');
+
+        this.sideA.append(this.goalA);
+        this.sideB.append(this.goalB);
+
         this.circle = $('<div class="field-circle"></div>');
+        
 
         this.field.append(this.sideA).append(this.sideB).append(this.circle);
         this.stadium.append(this.field);
+    }
+
+    createGoal(x) {
+        this['goalPost'+x+'1'] = $('<div class="goal'+x+' goalPost goalPost1"></div>');
+        this['goalPost'+x+'2'] =  $('<div class="goal'+x+' goalPost goalPost2"></div>');
+        let goal = $('<div class="goal"></div>');
+        let goalInside = $('<div class="goal-inside"></div>');
+        goal.append(goalInside.append(this['goalPost'+x+'1']).append(this['goalPost'+x+'2']));
+        return goal;
     }
 
     setSize(length, width) {
@@ -45,11 +72,29 @@ class Stadium
         return this;
     }
 
+    setGoalSize(size) {
+        this.goalSize = size;
+        return this;
+    }
+
     updateView() {
         this.stadium.css({
             'width':this.length+'px',
             'height':this.width+'px',
         });
+
+        let goalTop = this.width/2 - this.goalSize/2
+
+        this.goalA.css({
+            height: this.goalSize+'px',
+            top: goalTop+'px',
+        });
+
+        this.goalB.css({
+            height: this.goalSize+'px',
+            top: goalTop+'px',
+        });
+
         return this;
     }
 
@@ -66,22 +111,28 @@ class Stadium
     }
 
     calcPositions() {
+        // field position
         let x0 = this.field.position().left;
         let y0 = this.field.position().top;
         this.fieldPosition = {
             x: x0,
             y: y0,
         }
+
+        // center position
         this.centerPosition = {
             x: this.length/2 + x0,
             y: this.width/2 + y0,
         }
         console.log("field center: "+JSON.stringify(this.centerPosition));
+
+        // goals positions
+        
     }
 
     isBallOnField() {
         let p = this.ball.getPosition();
-        console.log("ball position: "+JSON.stringify(p));
+        //console.log("ball position: "+JSON.stringify(p));
         return p.x < this.fieldPosition.x - this.ball.r/2
             || p.x > this.fieldPosition.x + this.length + this.ball.r/2
             || p.y < this.fieldPosition.y - this.ball.r/2
